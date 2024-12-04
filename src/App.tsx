97bc9e8aa0ede6ab467ea88badddd9f2a87d1aa4
ianfefe <felix.ian@estudante.ufjf.br>
2024-12-04 19:39:56 -0300

    estilizacao

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,10 @@ function App() {
   return (
   <div>
     <Grade>
-      <Materia disciplina="DCC207" nome="Desenvolvimento Web Front-End" turma='A' dia='segunda-feira' inicio={new Date()} fim={new Date()}></Materia>
+      <Materia disciplina="DCC013" nome="Estrutura de Dados" turma='A' dia='Segunda-feira' inicio={new Date()} fim={new Date()}></Materia>
+      <Materia disciplina="DCC013" nome="Estrutura de Dados" turma='A' dia='Terca-feira' inicio={new Date()} fim={new Date()}></Materia>
+      <Materia disciplina="DCC207" nome="Desenvolvimento Web Front-End" turma='A' dia='Segunda-feira' inicio={new Date()} fim={new Date()}></Materia>
+      <Materia disciplina="DCC207" nome="Desenvolvimento Web Front-End" turma='A' dia='Quinta-feira' inicio={new Date()} fim={new Date()}></Materia>
     </Grade>
   </div> 
   )
